ae930c2bf9b0885c6c10f310e08225bb9d769979
BBrianhuynh <92127341+BBrianhuynh@users.noreply.github.com>
2023-02-22 12:39:28 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import Playlists from './components/Playlists'
+const App = () => {
+
+  return (
+    <div className="App">
+      <Playlists />
+    </div>
+  )
+}
+export default App
